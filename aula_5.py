af0f7ea8025c7505766aee6b7cdf5a57e37cec26
Yasmyn <yasmyyynn@gmail.com>
2024-11-23 17:01:36 -0300

    Add files via upload

---
+++ aula_5.py
@@ -0,0 +1,59 @@
+#1 
+print('Boas-vindas!')
+
+#2
+a1 = 2
+situacao= a1 >=5
+print('A resposta é:', situacao)
+
+#3
+n1 = 5
+n4 = 5
+multiplicacao = n1 * n4 
+print(multiplicacao)
+
+#4
+m1 = 3
+m2 = 8
+divisao = m1 / m2
+print (divisao)
+
+
+#5
+n2 = 6
+n5 = 9
+subtracao = n2 - n5 
+print(subtracao)
+
+#6 
+y1 = 2
+y5 = 4
+divisao =  y1 / y5
+print(divisao)
+
+#7
+q1 = 2
+q2 = 3
+q3 = 2
+q4 = 5
+multiplicacao= (q1 * q2 * q3 * q4)
+print(multiplicacao)
+
+#8
+o1 = 2 
+print(o1*2)
+
+
+#9
+f1 = input('Digite um numero:')
+f2 = input('Digite um numero:')
+soma = (f1 + f2)
+print(soma)
+
+#10
+nome = input('Nome:')
+idade = input('Idade:')
+estadocivil = input('Estado civil:')
+print('Olá', nome, idade, 'anos', estadocivil)
+
+
